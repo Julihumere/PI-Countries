c95f35796d4ab2e47128e81a3970e1895beff1a4
Julihumere <julihumereuner@gmail.com>
2023-06-06 18:01:20 -0300

    refactoring

--- api/index.js
+++ api/index.js
@@ -1,27 +1,8 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const axios = require("axios");
 const { Country } = require("./src/db");
 const { getApi } = require("./src/Controllers/Controllers");
+require("dotenv").config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
@@ -31,7 +12,7 @@ conn.sync({ force: true }).then(async () => {
 
     const SaveDb = await Country.bulkCreate(info);
   }
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
